e789e077fb6f9f8ec846d704862df355045d76b2
Nolan Nichols <nolan.nichols@gmail.com>
2013-11-16 11:41:17 -0800

    pl: making pylint happy

--- doc2hub/gd2gh.py
+++ doc2hub/gd2gh.py
@@ -24,35 +24,41 @@ import subprocess
 import gdata.docs.data
 import gdata.docs.client
 
+
 class Converter(object):
 
     def __init__(self, authconfig_file):
-        config = None
         with open(authconfig_file, 'rt') as fp:
-            config=json.load(fp)
+            config = json.load(fp)
         if config is None:
             raise ValueError('Config is empty')
         self._config = config
         self._client = gdata.docs.client.DocsClient(source='Doc2Hub')
         self._client.ssl = True  # Force all API requests through HTTPS
-        self._client.http_client.debug = False  # Set to True for debugging HTTP requests
-        self._client.ClientLogin(config['gmail_username'], config['gmail_password'], self._client.source);
+        self._client.http_client.debug = False  # True for HTTP debugging
+        self._client.ClientLogin(config['gmail_username'],
+                                 config['gmail_password'],
+                                 self._client.source)
 
     def print_feed(self, regexp=None):
         feed = self._client.GetDocList()
         print '\n'
         if not feed.entry:
-          print 'No entries in feed.\n'
+            print 'No entries in feed.\n'
         for entry in feed.entry:
             name = entry.title.text.encode('UTF-8')
             if re.match(regexp, name):
-                print entry.title.text.encode('UTF-8'), entry.GetDocumentType(), entry.resource_id.text
+                print entry.title.text.encode('UTF-8'),\
+                    entry.GetDocumentType(), \
+                    entry.resource_id.text
                 # List folders the document is in.
                 for folder in entry.InFolders():
                     print folder.title
 
     def get_entries(self, docname):
-        feed = self._client.GetAllResources(uri='/feeds/default/private/full?title=%s&title-exact=false&max-results=5'%docname)
+        uri_base = '/feeds/default/private/full'
+        query = '?title=%s&title-exact=false&max-results=5' % docname
+        feed = self._client.GetAllResources(uri=uri_base + query)
         if not feed:
             raise ValueError('No entries in feed.')
         return feed
@@ -74,9 +80,10 @@ class Converter(object):
                                     shell=True,
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE)
-            o,e = proc.communicate()
+            o, e = proc.communicate()
             if 'revision:' in o:
-                starting_revision = int(o.split('revision:')[-1].split(' ')[-1])
+                rev_split = o.split('revision:')
+                starting_revision = int(rev_split[-1].split(' ')[-1])
         else:
             os.mkdir(dirname)
             os.chdir(dirname)
@@ -97,13 +104,17 @@ class Converter(object):
                                           {'exportFormat': 'zip'})
             os.system('rm -rf images')
             os.system('unzip -o /tmp/%s.zip'%docname)
-            os.system('pandoc -f html -t rst -o %s.rst %s.html'%(docname, docname))
+            os.system('pandoc -f html -t rst -o %s.rst %s.html' % (docname,
+                                                                   docname))
             if os.path.exists('images'):
-                os.system('git add %s.rst images/*.png'%docname)
+                os.system('git add %s.rst images/*.png' % docname)
             else:
-                os.system('git add %s.rst'%docname)
-            os.system('git commit --date=%s --author="%s <%s>" -m "revision: %d"'%(date, name, email, revision_number))
+                os.system('git add %s.rst' % docname)
+            git_commit = 'git commit --date=%s --author="%s <%s>" -m ' \
+                         '"revision:%d"'
+            format_commit = git_commit % (date, name, email, revision_number)
+            os.system(format_commit)
             if starting_revision > -1:
                 os.system('git push origin master')
-            print "finished converting revision: %d"%revision_number
+            print "finished converting revision: %d" % revision_number
         os.chdir(pwd)
